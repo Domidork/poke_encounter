4d6f0f77dfa2d88f9f5126a3adb38a9fe0e1c4b0
Domidork <dgakiddos@gmail.com>
2026-02-06 20:58:14 -0500

    added easy pokemon instantiation and upgraded printing stats

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ from functions import *
 from pokemon import pokemon
 
 
-bulbasaur = pokemon('bulbasaur')
-bulbasaur.print_stats()
+poke1 = pokemon('bulbasaur')
+print(poke1)
 
-
-
-# poke_data = get_pokemon_data('bulbasaur')
-# moves = get_moves(poke_data)
-# pokemon_moves = randomize_moves(moves)
-
-# print(f'Health: {poke_data['stats'][0]['base_stat']}')
-# for move in pokemon_moves:
-#     print(f'{move['name']}: {move.get('power', 0)}')
+poke2 = pokemon('charmander')
+print(poke2)
--- pokemon.py
+++ pokemon.py
@@ -2,11 +2,20 @@ from functions import *
 
 class pokemon():
     def __init__(self, name):
-        self.data = get_pokemon_data('bulbasaur')
+        self.data = get_pokemon_data(name)
         self.possible_moves = get_moves(self.data)
         self.moves = randomize_moves(self.possible_moves)
 
-    def print_stats(self):
-        print(f'Health: {self.data['stats'][0]['base_stat']}')
+    def __str__(self):
+        lines = []
+
+        lines.append(f'\n------{self.data['name'].capitalize()}------')
+        lines.append(f'Health: {self.data['stats'][0]['base_stat']}')
+        lines.append('Moves:')
+        
         for move in self.moves:
-            print(f'{move['name']}: {move.get('power', 0)}')
+            lines.append(f'\t{move['name']}: {move.get('power', 0)}')
+
+        lines.append("")
+
+        return '\n'.join(lines)
