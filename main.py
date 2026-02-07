a36c64996f7b9d0746fbeb6197953191a8652920
Domidork <dgakiddos@gmail.com>
2026-02-06 21:39:41 -0500

    added basic fighting mechanics; using moves and taking damage

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ print(poke1)
 
 poke2 = pokemon('charmander')
 print(poke2)
+
+
+use_move1 = poke1.use_rand_move(poke2)
+print(use_move1[2])
+use_move2 = poke2.use_rand_move(poke1)
+print(use_move2[2])
--- pokemon.py
+++ pokemon.py
@@ -1,10 +1,13 @@
 from functions import *
+import random
 
 class pokemon():
     def __init__(self, name):
         self.data = get_pokemon_data(name)
+        self.name = name
         self.possible_moves = get_moves(self.data)
         self.moves = randomize_moves(self.possible_moves)
+        self.health = self.data['stats'][0]['base_stat']
 
     def __str__(self):
         lines = []
@@ -14,8 +17,21 @@ class pokemon():
         lines.append('Moves:')
         
         for move in self.moves:
-            lines.append(f'\t{move['name']}: {move.get('power', 0)}')
+            lines.append(f'\t{move['name']}: {move.get('power', 0) or 0}')
 
         lines.append("")
 
         return '\n'.join(lines)
+    
+    def take_damage(self, other, inflicted):
+        self.health -= inflicted
+        return f'{other.name} dealt {inflicted}dmg to {self.name}!\n{self.name} is now at {self.health}hp!'
+
+    def use_rand_move(self, other):
+        choice = random.choice(self.moves)
+        move_name = choice['name']
+        move_dmg = choice.get('power', 0) or 0
+        # Inflicts the damage on the target pokemon
+        dmg_message = other.take_damage(self, move_dmg)
+        # --------------
+        return (move_name, move_dmg, dmg_message)
