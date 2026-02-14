d8e59f9cc7a31b050300f4ebe617bdad2d1500a8
Domidork <dgakiddos@gmail.com>
2026-02-14 11:34:55 -0500

    added primitive realtime support + tag support and better fight formatting

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import sys
 import os
 import subprocess
 from functions import *
+from sefuncs import rtsleep
 from pokemon import pokemon
 
+# Where the actual arguments start after tags
+argstart = 1
 if '--help' in sys.argv:
     print(help())
     sys.exit()
@@ -15,11 +18,16 @@ if '--link' in sys.argv:
     else:
         print('You have already linked this program to home!')
     sys.exit()
-if len(sys.argv) == 3:
-    poke_name_1 = sys.argv[1]
-    poke_name_2 = sys.argv[2]
-elif len(sys.argv) == 2:
-    if sys.argv[1] == 'random':
+if '-rt' in sys.argv:
+    realtime = True
+    argstart += 1
+else:
+    realtime = False
+if len(sys.argv) == argstart + 2:
+    poke_name_1 = sys.argv[argstart]
+    poke_name_2 = sys.argv[argstart + 1]
+elif len(sys.argv) == argstart + 1:
+    if sys.argv[argstart] == 'random':
         poke_name_1 = choose_random_pokemon()
         poke_name_2 = choose_random_pokemon()
     else:
@@ -35,7 +43,11 @@ print(poke1)
 
 poke2 = pokemon(poke_name_2)
 print(poke2)
+
+turn = 1
 while poke1.health > 0 and poke2.health > 0:
+    print(f'--------Turn {turn}--------')
+    rtsleep(realtime)
     move_order = first_mover(poke1, poke2)
     use_move1 = move_order[0].use_rand_move(move_order[1])
     dmg_return1 = use_move1[2]
@@ -44,6 +56,8 @@ while poke1.health > 0 and poke2.health > 0:
         break
     use_move2 = move_order[1].use_rand_move(move_order[0])
     dmg_return2 = use_move2[2]
+    rtsleep(realtime)
     print(dmg_return2[0])
     if dmg_return2[1] == False:
         break
+    turn += 1
---
+++ sefuncs.py
@@ -0,0 +1,5 @@
+import time
+
+def rtsleep(realtime):
+    if realtime:
+        time.sleep(1)
